4e6915edfc8ea4c9133513194ece98a3506561e2
FifiLen <filippraca02@gmail.com>
2025-10-21 13:08:54 +0200

    Implemented hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main>
+      <NavBar />
+      <Hero />
+    </main>
+  );
 };
 export default App;
